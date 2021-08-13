4f2f4a816dcc36d845167e2d73c87895ed2b5c53
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-08-12 22:29:18 -0500

    added comments on breaking up what needs to be done, also fixed js on html of where it was called at

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 //day out of the year
 let clock = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").text(dayYear);
+$("currentDay").html(clock);
 
 
+// Need to get current # of hours between 8-5 
+// Need to make a custom array with 3 elements, hour, event, and save button in local storage
