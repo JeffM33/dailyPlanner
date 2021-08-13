2ad29f8f551686ebf165c1b4c67cd4025f2e24d6
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-08-12 23:45:50 -0500

    stated what was wrong with code and what i need to do to fix it.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,24 @@
-//day out of the year
+//day out of the year tried to setup to display in header
 let clock = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("currentDay").html(clock);
 
-let eightEvent = document.getElementById("#8am")
+// Setup elements for different hours need to be able to call these and save the desc in each one
+let eightEvent = document.getElementById("#8am");
+let nineEvent = document.getElementById("#9am");
+let tenEvent = document.getElementById("#10am");
+let elevenEvent = document.getElementById("#11am");
+let twelveEvent = document.getElementById("#12pm");
+let oneEvent = document.getElementById("#1pm");
+let twoEvent = document.getElementById("#2pm");
+let threeEvent = document.getElementById("#3pm");
+let fourEvent = document.getElementById("#4pm");
+let fiveEvent = document.getElementById("#5pm");
+
 let saveBtn = document.querySelector("#saveBtn");
 let descriptionText = document.querySelector("#text");
 
 // Need to get current hour and set class to past present future 
+// need to pass correct element variable through function to compare to current time?
 function time() {
     let currentTime = moment().hour();
 
@@ -18,8 +30,9 @@ function time() {
         this.addClass("past");
     }
 }
-// Need to make a custom array with 3 elements, hour, event, and save button in local storage
 
+// added event listeners to save btn but i need to call on specific rows or ids so ccorrect desc gets updated
+// Also trying to save in information in local storage, need to save with correct id
 saveBtn.addEventListener("click", function(event) {
     event.preventDefault();
 
@@ -29,6 +42,7 @@ saveBtn.addEventListener("click", function(event) {
     renderMessage();
 });
 
+// need to fix so event will render and show up.
 function renderMessage() {
     var desc = JSON.parse(localStorage.getItem("description"));
     if(desc !== null){
